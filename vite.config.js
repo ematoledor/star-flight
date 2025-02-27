d0af454fc2c2ae39b868ed83ba35a25037ed1963
ematol <ematolr@gmail.com>
2025-02-27 10:02:32 -0600

    Complete overhaul of build configuration for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -2,27 +2,13 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/',
+  base: './',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-    sourcemap: true,
-    assetsDir: 'assets',
-    minify: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html')
-      }
-    }
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src')
-    },
+    minify: true
   },
   server: {
-    host: true,
-    open: true,
     port: 3000
   }
 }); 
